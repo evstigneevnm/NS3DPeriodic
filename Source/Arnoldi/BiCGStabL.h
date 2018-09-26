094c168f153e36f00ac9103675416ddb9ee6b586
evstigneevnm <evstigneevnm@gmail.com>
2018-09-26 09:02:30 +0300

    preliminary throw added

--- Source/Arnoldi/BiCGStabL.h
+++ Source/Arnoldi/BiCGStabL.h
@@ -10,6 +10,8 @@
 #include "memory_operations.h"
 #include "Matrix_Vector_emulator.h"
 
+#include <iostream> //for exaptions
+
 
 int BiCGStabL(cublasHandle_t handle, int L, int N, user_map_vector Axb, void *user_struct, real *x, real* RHS, real *tol, int *Iter, bool verbose, unsigned int skip=500);
 	
